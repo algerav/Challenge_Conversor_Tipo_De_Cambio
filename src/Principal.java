083ad13c539b3cb9bb4921e35e50ca08c73b7dfb
Alfredo Gerardo <agerardo172@gmail.com>
2025-07-20 14:08:12 -0600

    Documentación 1

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,7 @@
+//El programa convierte el tipo de cambio de una cantidad entre distintas monedas como
+//Dólares (USD), Pesos Mexicanos (MXN), Peso Argentino (ARS), Real Brasileño (BRL) y Peso Colombiano (COP).
+
+
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.io.IOException;
 import java.time.LocalDate;
@@ -6,6 +10,9 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
+
+//La clase "Principal" ejecuta el programa dentro de un loop "while" dónde se pide al usuario que seleccione la
+//operación a convertir y el monto a convertir.
 public class Principal {
     public static void main(String[] args) {
         Scanner leeOpcion = new Scanner(System.in);
@@ -17,34 +24,39 @@ public class Principal {
                     ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
                     Bienvenido/a al Conversor de Tipo de Cambio :)
                     
-                    1) Dólar =====>>> Peso Mexicano
-                    2) Peso Mexicano =====>>> Dólar
-                    3) Dólar =====>>> Peso Argentino
-                    4) Peso Argentino  =====>>> Dólar
-                    5) Dólar =====>>> Real Brasileño
-                    6) Real Brasileño =====>>> Dólar
-                    7) Dólar =====>>> Peso Colombiano
-                    8) Peso Colombiano =====>>> Dólar
+                    1) Dólar (USD) =====>>> Peso Mexicano (MXN)
+                    2) Peso Mexicano (MXN) =====>>> Dólar (USD)
+                    3) Dólar (USD) =====>>> Peso Argentino (ARS)
+                    4) Peso Argentino (ARS) =====>>> Dólar (USD)
+                    5) Dólar (USD) =====>>> Real Brasileño (BRL)
+                    6) Real Brasileño (BRL) =====>>> Dólar (USD)
+                    7) Dólar (USD) =====>>> Peso Colombiano (COP)
+                    8) Peso Colombiano (COP) =====>>> Dólar (USD)
                     9) Salir
                     
                     Elija el número de opción deseada?
                     ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
                     """);
 
+            //La sentencia "try" evalua las distintas opciones que el usuario selecciona asegurando que sean opciones válidas,
+            //de lo contrario la sentencia "catch" mostrará que la información ingresada no es correcta y que se seleccione alguna
+            //opción válida
             try {
-                var opcionUsuario = Integer.valueOf(leeOpcion.nextLine());
-                var base = "";
-                var target = "";
+                var opcionUsuario = Integer.valueOf(leeOpcion.nextLine());  //opcionUsuario tendrá la opción seleccionada por el usuario que será un valor tipo Int.
+                var base = ""; //Inicialización de las variables base correspondiente al parámetro base_code de la API "ExchangeRate-API" utilizada en la clase "Consulta.java"
+                var target = "";//Inicialización de las variables target correspondiente al parámetro target_code de la API "ExchangeRate-API" utilizada en la clase "Consulta.java"
+
+                //La sentencia "if" evalúa la operación a realizar en el convertidor:
                 if (opcionUsuario.equals(1)) {
                     System.out.println("Ingrese la cantidad de Dólares USD a convertir a Pesos MX: ");
                     base = "USD";
                     target = "MXN";
                 } else if (opcionUsuario.equals(2)) {
-                    System.out.println("Ingrese la cantidad de Pesos Mexicanos a convertir a Dólares USD: ");
+                    System.out.println("Ingrese la cantidad de Pesos Mexicanos MXN a convertir a Dólares USD: ");
                     base = "MXN";
                     target = "USD";
                 } else if (opcionUsuario.equals(3)) {
-                    System.out.println("Ingrese la cantidad de Dólares a convertir a Pesos Argentinos ARS: ");
+                    System.out.println("Ingrese la cantidad de Dólares USD a convertir a Pesos Argentinos ARS: ");
                     base = "USD";
                     target = "ARS";
                 } else if (opcionUsuario.equals(4)) {
@@ -74,7 +86,8 @@ public class Principal {
                     System.out.println("Opción inválida. Por favor, elija una opción válida.");
                     continue;
                 }
-                var montoAConvertir = Double.valueOf(leeCantidad.nextLine());
+
+                var montoAConvertir = Double.valueOf(leeCantidad.nextLine()); //montoAConvertir tendrá el monto ingresado por el usuario que será un valor tipo Double.
 
 
 
